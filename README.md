091b88d22614f9c419a615e755b9a5394fa44967
Bertrand Fan <bertrand@fan.net>
2013-06-26 23:08:59 -0700

    Initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# glitch-assets-parser
+
+Parses Glitch assets (food and drink) metadata into a JSON format
+
+I was playing around with the [Glitch assets](http://www.glitch.com/licensing/) but I wanted a lookup of all the food and drink items with a path to their icons. This parses the Glitch encyclopedia and assets file structure to build that index.
+
+If you just want the end result of this, take a look at `items.json`.
+
+## Usage
+
+
+```
+usage: parser.py [-h] [-f FILENAME]
+
+Parses Glitch assets (food and drink) metadata into a JSON format
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FILENAME, --filename FILENAME
+                        file to output to
+```
+
---
+++ parser.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+import urllib2
+from BeautifulSoup import BeautifulSoup
+import re
+import os, os.path
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='Parses Glitch assets (food and drink) metadata into a JSON format')
+parser.add_argument('-f', '--filename', type=argparse.FileType('w'), help='file to output to')
+
+args = parser.parse_args()
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+
+output = {}
+
+categories = ["food", "drinks"]
+
+for category in categories:
+
+	txt = open(script_path + '/html/items_' + category + '.html')
+	html = txt.read()
+
+	soup = BeautifulSoup(html)
+
+	el_items_list = soup.find("ul", {"class": "items-list"})
+	el_items = el_items_list.findAll("li")
+
+	items = {}
+
+	for el_item in el_items:
+
+		item = {}
+
+		link = el_item.find('a')
+
+		match = re.search('/items/(.*)/(.*)/', link['href'])
+		item_id = match.group(2)
+
+		item['name'] = el_item.find('span', {"class" : "item-name"}).string
+		item['description'] = el_item['title']
+
+		item_html = open(script_path + '/html/items_' + category + '_' + item_id + '.html').read()
+		item_soup = BeautifulSoup(item_html)
+		el_asset_list = item_soup.find('table', {"class" : "asset_list"})
+		el_asset_links = el_asset_list.findAll('a')
+		el_asset_link = el_asset_links[0]
+
+		item_match = re.search('/2012-12-06/(.*)__', el_asset_link['href'])
+		directory_name = item_match.group(1)
+
+		for dirname, dirnames, filenames in os.walk(script_path + '/glitch-assets/' + directory_name + '/'):
+		    for filename in filenames:
+		    	if "iconic" in filename:
+		    		item['asset_path'] = 'glitch-assets/' + directory_name + '/' + filename
+
+		items[item_id] = item
+
+	output[category] = items
+
+json_output = json.dumps(output, sort_keys=True, indent=4, separators=(',', ': '))
+
+if args.filename != None:
+	args.filename.write(json_output)
+	args.filename.close()
+else:
+	print json_output
